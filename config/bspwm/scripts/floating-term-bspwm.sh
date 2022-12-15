14616fd4d8df76d6bffe44edd9e4b04869d85ecb
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2022-12-15 04:58:50 +0100

    Delete config/bspwm directory
    bspwm removed is going to be replaced

--- config/bspwm/scripts/floating-term-bspwm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bspc rule -a \* -o state=floating 
-bspc desktop -f '^1'
-kitty -e $SHELL -c "cd && $HOME/.config/neofetch/launch-neofetch.sh && $SHELL"
-
--- config/bspwm/scripts/screenshot_region.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-sleep 0.2 ; scrot -s 'ScreenShot_%Y-%m-%d_at_%I:%M:%S-%p.png' -e 'mv $f ~/Pictures/Screenshots/'
