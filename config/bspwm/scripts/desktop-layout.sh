977cf6c05219b46e7a941ab9d0bd3725e460c530
RedBlizard <108489214+RedBlizard@users.noreply.github.com>
2023-01-15 01:17:48 +0100

    Added new BSPWM dir
    New dir with fixes

---
+++ config/bspwm/scripts/desktop-layout.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# sets the layout of each desktop by redblizard
+
+bsp-layout set tall I
+
+bsp-layout set rtall II
+
+bsp-layout set tall III
+
+bsp-layout set tall IV
+
+bsp-layout set tall V
+
+bsp-layout set tall VI
+
+bsp-layout set tall VII
+
+bsp-layout set monocle VIII
